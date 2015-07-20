b9f07c35ac08802f5dd42e57c5d63dab8c41d226
icstunna <mdelostr@gmail.com>
2015-07-20 15:24:54 -0700

    first commit models controllers index routes also added rotten tomatoe api key

---
+++ db/migrate/20150720214241_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.string :name
+      t.string :email
+      t.text :content
+      t.belongs_to :movie
+
+      t.timestamps null: false
+    end
+  end
+end
