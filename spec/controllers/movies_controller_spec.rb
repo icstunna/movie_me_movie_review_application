254f6f4a6b1f69023dd28bc51cbfed1872f3da96
icstunna <mdelostr@gmail.com>
2015-07-21 10:26:37 -0700

    set up rspec environment and allowed firt test of rendering to show

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
+  before :all do
+    Movie.destroy_all
+    @movie = Movie.create!(title: "Fast 5",
+                           genre: "Action",
+                           thumbnail: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Fast_Five_logo.png",
+                           url: "http://www.fastfive.com/",
+                           release_date: Date.new(2011, 4, 29))
+  end
+
+  describe "#show" do
+    it "shows the movie page" do
+      get :show, id: @movie.id
+      expect(response).to render_template(:show)
+    end
+  end
 
 end
